0006dc210ca4b01c75b31dc73af6024086d02f82
unknown <hp@AbidHaleem.(none)>
2012-06-27 23:28:12 +0530

    added form

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<center>
-<form action = "index.php" method ="post">
-username : <input type="text" name = "user">
-password : <input type="password" name="pass">
-<input type="submit>
+<font face="verdana,arial" size=-1>
+<center><table cellpadding='2' cellspacing='0' border='0' id='ap_table'>
+<tr><td bgcolor="blue"><table cellpadding='0' cellspacing='0' border='0' width='100%'><tr><td bgcolor="blue" align=center style="padding:2;padding-bottom:4"><b><font size=-1 color="white" face="verdana,arial"><b>Enter your login and password</b></font></th></tr>
+<tr><td bgcolor="white" style="padding:5"><br>
+<form method="post" action="http://www.authpro.com/auth/deluxe/" name="aform">
+<input type="hidden" name="action" value="login">
+<input type="hidden" name="hide" value="">
+<center><table border='0'>
+<tr><td><font face="verdana,arial" size=-1>Login:</td><td><input type="text" name="login"></td></tr>
+<tr><td><font face="verdana,arial" size=-1>Password:</td><td><input type="password" name="password"></td></tr>
+<tr><td><font face="verdana,arial" size=-1>&nbsp;</td><td><font face="verdana,arial" size=-1><input type="submit" value="Enter"></td></tr>
+<tr><td colspan=2><font face="verdana,arial" size=-1>&nbsp;</td></tr>
+</table></center>
 </form>
-</center>
+</td></tr></table></td></tr></table>
