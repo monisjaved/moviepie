421a98ee81ee8b3c0c8e09604a5e435dc62879ca
unknown <hammad haleem@shahidakhan-PC.(none)>
2012-07-02 15:44:59 +0530

    edits

--- template.php
+++ template.php
@@ -11,6 +11,83 @@
 	<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"> </script>
 	<script src="js/tabs.js" type="text/javascript"> </script>
 	<script src="js/search.js" type="text/javascript"> </script>
+	<script type="text/javascript">
+            function updateStatus(){
+                var status  =   document.getElementById('status').value;
+                
+                $.ajax({
+                    type: "POST",
+                    url: "<?=$fbconfig['baseUrl']?>/ajax.php",
+                    data: "status=" + status,
+                    success: function(msg){
+                        alert(msg);
+                    },
+                    error: function(msg){
+                        alert(msg);
+                    }
+                });
+            }
+            function updateStatusViaJavascriptAPICalling(){
+                var status  =   document.getElementById('status').value;
+                    FB.api('/me/feed', 'post', { message: status }, function(response) {
+                        if (!response || response.error) {
+                             alert('Error occured');
+                        } else {
+                             alert('Status updated Successfully');
+                        }
+                   });
+            }
+            function streamPublish(name, description, hrefTitle, hrefLink, userPrompt){        
+                FB.ui({ method : 'feed', 
+                        message: userPrompt,
+                        link   :  hrefLink,
+                        caption:  hrefTitle
+                      });
+                    //http://developers.facebook.com/docs/reference/dialogs/feed/
+        
+                /* 
+                 * Deprecated code
+                FB.ui(
+                {
+                    method: 'stream.publish',
+                    message: '',
+                    attachment: {
+                        name: name,
+                        caption: '',
+                        description: (description),
+                        href: hrefLink
+                    },
+                    action_links: [
+                        { text: hrefTitle, href: hrefLink }
+                    ],
+                    user_prompt_message: userPrompt
+                },
+                function(response) {
+
+                }); */
+            }
+            function publishStream(){
+                streamPublish("Stream Publish", 'Thinkdiff.net is simply awesome. I just learned how to develop Iframe+Jquery+Ajax base facebook application development using php sdk 3.0. ', 'Checkout the Tutorial', 'http://wp.me/pr3EW-sv', "Demo Facebook Application Tutorial");
+            }
+            function increaseIframeSize(w,h){
+                var obj =   new Object;
+                obj.width=w;
+                obj.height=h;
+                FB.Canvas.setSize(obj);
+            }
+
+            function newInvite(){
+                 var receiverUserIds = FB.ui({ 
+                        method : 'apprequests',
+                        message: 'come on man checkout my application. visit http://thinkdiff.net',
+                 },
+                 function(receiverUserIds) {
+                          console.log("IDS : " + receiverUserIds.request_ids);
+                        }
+                 );
+                 //http://developers.facebook.com/docs/reference/dialogs/requests/
+            }
+        </script>
 </head>
 <body>
 
