38c078c3fad44dcb1850b0a732cae212709c3037
unknown <hammad haleem@shahidakhan-PC.(none)>
2012-07-02 15:51:41 +0530

    edits

--- template.php
+++ template.php
@@ -27,49 +27,16 @@
                     }
                 });
             }
-            function updateStatusViaJavascriptAPICalling(){
-                var status  =   document.getElementById('status').value;
-                    FB.api('/me/feed', 'post', { message: status }, function(response) {
-                        if (!response || response.error) {
-                             alert('Error occured');
-                        } else {
-                             alert('Status updated Successfully');
-                        }
-                   });
-            }
+            
             function streamPublish(name, description, hrefTitle, hrefLink, userPrompt){        
                 FB.ui({ method : 'feed', 
                         message: userPrompt,
                         link   :  hrefLink,
                         caption:  hrefTitle
                       });
-                    //http://developers.facebook.com/docs/reference/dialogs/feed/
-        
-                /* 
-                 * Deprecated code
-                FB.ui(
-                {
-                    method: 'stream.publish',
-                    message: '',
-                    attachment: {
-                        name: name,
-                        caption: '',
-                        description: (description),
-                        href: hrefLink
-                    },
-                    action_links: [
-                        { text: hrefTitle, href: hrefLink }
-                    ],
-                    user_prompt_message: userPrompt
-                },
-                function(response) {
-
-                }); */
+                   
             }
-            function publishStream(){
-                streamPublish("Stream Publish", 'Thinkdiff.net is simply awesome. I just learned how to develop Iframe+Jquery+Ajax base facebook application development using php sdk 3.0. ', 'Checkout the Tutorial', 'http://wp.me/pr3EW-sv', "Demo Facebook Application Tutorial");
-            }
-            function increaseIframeSize(w,h){
+           function increaseIframeSize(w,h){
                 var obj =   new Object;
                 obj.width=w;
                 obj.height=h;
@@ -79,14 +46,13 @@
             function newInvite(){
                  var receiverUserIds = FB.ui({ 
                         method : 'apprequests',
-                        message: 'come on man checkout my application. visit http://thinkdiff.net',
+                        message: 'Friends check out this app ! http://apps.facebook.com/knowyourcollege',
                  },
                  function(receiverUserIds) {
                           console.log("IDS : " + receiverUserIds.request_ids);
                         }
                  );
-                 //http://developers.facebook.com/docs/reference/dialogs/requests/
-            }
+              }
         </script>
 </head>
 <body>
