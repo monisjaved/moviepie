ffd50f93ec77071e486c23ac72e0ce90723a5d8e
hammad <hammadhaleem@gmail.com>
2012-07-15 08:08:05 +0530

    Dbtest

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysql_connect("www.engineerinme.com","engineer","kgggdkp1992");
+$con = mysql_connect("engineerinme.com","engineer","kgggdkp1992");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
