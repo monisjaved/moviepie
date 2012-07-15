ed83b2405d8afd06449ffa22607eeec418a9228b
hammad <hammadhaleem@gmail.com>
2012-07-15 22:02:26 +0530

    te

--- include/suggested.php
+++ include/suggested.php
@@ -29,9 +29,10 @@ if($user){
 		d($list);
 		for($j=0;$j<$i;$j++)
 		{	
-		$url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=".$list[$j]."&page_limit=5&page=1&apikey=uuacu746nquzs3f2679dcyv6";
-		$url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=HarryPotter&page_limit=5&page=1&apikey=uuacu746nquzs3f2679dcyv6";
-			echo $url ;
+		$arra=explode ("",$list[$j]);
+		$url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=".$arra[0]."&page_limit=5&page=1&apikey=uuacu746nquzs3f2679dcyv6";
+		//$url="http://api.rottentomatoes.com/api/public/v1.0/movies.json?q=HarryPotter&page_limit=5&page=1&apikey=uuacu746nquzs3f2679dcyv6";
+		//	echo $url ;
 			$referer=$url ; 
 			$json=getPage($url, $referer, $timeout, $header);
 			$data=json_decode($json);
