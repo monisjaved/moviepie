7cbcd0717401bb9fead9fc9f97361c18bfcdddf1
hammad <hammadhaleem@gmail.com>
2012-07-15 08:25:48 +0530

    we

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-$con = mysql_connect("saint.nseasy.com:3307","engineer","kgggdkp1992");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-
-// some code
+include "config.php"
 ?>
