eca606351ce7dd5d08a66a1f85cdb2ab95a3cc0f
hammad <hammadhaleem@gmail.com>
2012-07-15 08:31:37 +0530

    we

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
-include "config.php"
-$res=$dbh->Query("select * from movie ");
 
+ try
+        {
+            include "config.php";
+        } catch (Exception $e) {
+            echo "<pre>"; 
+			print_r($e);
+			echo "</pre>";
+        }
+ try
+        {
+            $res=$dbh->Query("select * from movie ");
+        } catch (Exception $e) {
+            echo "<pre>"; 
+			print_r($e);
+			echo "</pre>";
+        }
 echo "<pre>"; 
 print_r($res);
 echo "</pre>";
