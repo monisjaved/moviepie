5a07274ffe7f4de74e109f305fc83e54988d5954
Moonis Javed <monis.javed@gmail.com>
2012-07-24 23:26:23 +0530

    div

--- template/movies.php
+++ template/movies.php
@@ -13,7 +13,7 @@ $mid = $_GET['mid'];
 	{
 		background-image:url('http://cf2.imgobject.com/t/p/w500<?php echo $j11->backdrop_path ; ?>');
 		background-repeat:no-repeat;
-		background-position:300px 200px;
+		background-position:380px 1500px;
 		background-color:#cccccc;
 	}
 </style>
